3ede584cb57439f04eec148873280f2ba1bd6cb7
kitajima.yuto <kitajima.yuto@furyu.jp>
2023-10-26 00:12:01 +0900

    wip attempt to use serverless modules as websocket server

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,52 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {Construct} from 'constructs';
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import {WebSocketApi, WebSocketStage} from "@aws-cdk/aws-apigatewayv2-alpha";
+import {WebSocketLambdaIntegration} from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const onConnect = new NodejsFunction(this, 'onConnect', {
+            functionName: 'onConnect',
+        });
+        const onMessage = new NodejsFunction(this, 'onMessage', {
+            functionName: 'onMessage',
+        });
+        const onDisconnect = new NodejsFunction(this, 'onDisconnect', {
+            functionName: 'onDisconnect',
+        });
+        const defaultFunc = new NodejsFunction(this, 'default', {
+            functionName: 'default',
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const api = new WebSocketApi(this, 'api', {
+            apiName: 'planningPokerServer',
+            connectRouteOptions: {
+                integration: new WebSocketLambdaIntegration('connectIntegration', onConnect),
+            },
+            disconnectRouteOptions: {
+                integration: new WebSocketLambdaIntegration('disconnectIntegration', onDisconnect),
+            },
+            defaultRouteOptions: {
+                integration: new WebSocketLambdaIntegration('defaultIntegration', defaultFunc),
+            },
+        });
+
+        api.addRoute('send-message', {
+            integration: new WebSocketLambdaIntegration('messageIntegration', onMessage),
+        });
+        api.grantManageConnections(onConnect);
+        api.grantManageConnections(onDisconnect);
+        api.grantManageConnections(defaultFunc);
+        api.grantManageConnections(onMessage);
+
+        new WebSocketStage(this, 'planningPokerServerStage', {
+            stageName: 'v1',
+            webSocketApi: api,
+        });
+    }
 }
