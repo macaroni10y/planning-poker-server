fa8934c3ba357610ab81307456425cfee9fda48c
kitajima.yuto <kitajima.yuto@furyu.jp>
2023-11-03 23:56:48 +0900

    refactor

--- cdk/lib/planning-poker-server-stack.ts
+++ cdk/lib/planning-poker-server-stack.ts
@@ -1,53 +1,25 @@
 import {Construct} from 'constructs';
-import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
-import {WebSocketApi, WebSocketStage} from "@aws-cdk/aws-apigatewayv2-alpha";
-import {WebSocketLambdaIntegration} from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
-import {Stack, StackProps} from "aws-cdk-lib";
-import * as path from "path";
-import {AttributeType, BillingMode, Table} from "aws-cdk-lib/aws-dynamodb";
+import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs';
+import {WebSocketApi, WebSocketStage} from '@aws-cdk/aws-apigatewayv2-alpha';
+import {WebSocketLambdaIntegration} from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import {Stack, StackProps} from 'aws-cdk-lib';
+import * as path from 'path';
+import {AttributeType, BillingMode, Table} from 'aws-cdk-lib/aws-dynamodb';
 
 export class PlanningPokerServerStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        const nameToFunction = (name: string) =>
-            new NodejsFunction(this, name, {
-                entry: path.join(__dirname, `../src/functions/${name}/index.ts`),
-                functionName: name,
-            });
+        const functions = this.createLambdaFunctions([
+            'onConnect',
+            'joinRoom',
+            'resetRoom',
+            'submitCard',
+            'onDisconnect',
+            'default',
+        ]);
 
-        const [onConnect, joinRoom, resetRoom, submitCard, onDisconnect, defaultFunc]: NodejsFunction[]
-            = ['onConnect', 'joinRoom', 'resetRoom', 'submitCard', 'onDisconnect', 'default']
-            .map(nameToFunction);
-
-        const api = new WebSocketApi(this, 'api', {
-            apiName: 'planningPokerServer',
-            connectRouteOptions: {
-                integration: new WebSocketLambdaIntegration('connectIntegration', onConnect),
-            },
-            disconnectRouteOptions: {
-                integration: new WebSocketLambdaIntegration('disconnectIntegration', onDisconnect),
-            },
-            defaultRouteOptions: {
-                integration: new WebSocketLambdaIntegration('defaultIntegration', defaultFunc),
-            },
-        });
-
-        api.addRoute('joinRoom', {
-            integration: new WebSocketLambdaIntegration('joinRoomIntegration', joinRoom),
-        });
-        api.addRoute('resetRoom', {
-            integration: new WebSocketLambdaIntegration('resetRoomIntegration', resetRoom),
-        });
-        api.addRoute('submitCard', {
-            integration: new WebSocketLambdaIntegration('submitCardIntegration', submitCard)
-        });
-        api.grantManageConnections(onConnect);
-        api.grantManageConnections(joinRoom);
-        api.grantManageConnections(submitCard);
-        api.grantManageConnections(onDisconnect);
-        api.grantManageConnections(defaultFunc);
-        api.grantManageConnections(resetRoom);
+        const api = this.createWebSocketApi(functions);
 
         new WebSocketStage(this, 'planningPokerServerStage', {
             stageName: 'v1',
@@ -55,22 +27,64 @@ export class PlanningPokerServerStack extends Stack {
             autoDeploy: true,
         });
 
+        const table = this.createDynamoDBTable();
+
+        this.grantTablePermissions(table, functions);
+    }
+
+    private createLambdaFunction = (name: string): NodejsFunction => new NodejsFunction(this, name, {
+        entry: path.join(__dirname, `../src/functions/${name}/index.ts`),
+        functionName: name,
+    });
+
+    private createLambdaFunctions(names: string[]): Record<string, NodejsFunction> {
+        return names.reduce((acc, name) => {
+            acc[name] = this.createLambdaFunction(name);
+            return acc;
+        }, {} as Record<string, NodejsFunction>);
+    }
+
+    private createWebSocketApi(functions: Record<string, NodejsFunction>): WebSocketApi {
+        const api = new WebSocketApi(this, 'api', {
+            apiName: 'planningPokerServer',
+        });
+
+        const routes = [
+            {route: '$connect', func: 'onConnect'},
+            {route: '$disconnect', func: 'onDisconnect'},
+            {route: 'joinRoom', func: 'joinRoom'},
+            {route: 'resetRoom', func: 'resetRoom'},
+            {route: 'submitCard', func: 'submitCard'},
+            {route: '$default', func: 'default'},
+        ];
+
+        routes.forEach(({route, func}) => {
+            api.addRoute(route, {
+                integration: new WebSocketLambdaIntegration(`${func}Integration`, functions[func])
+            });
+            api.grantManageConnections(functions[func]);
+        });
+
+        return api;
+    }
+
+    private createDynamoDBTable = (): Table => {
         const table = new Table(this, 'planningPokerTable', {
             tableName: 'PlanningPoker',
             billingMode: BillingMode.PAY_PER_REQUEST,
             partitionKey: {name: 'roomId', type: AttributeType.STRING},
             sortKey: {name: 'clientId', type: AttributeType.STRING},
         });
+
         table.addGlobalSecondaryIndex({
-            indexName: "ClientIdIndex",
+            indexName: 'ClientIdIndex',
             partitionKey: {name: 'clientId', type: AttributeType.STRING},
         });
-        table.grantFullAccess(onConnect);
-        table.grantFullAccess(joinRoom);
-        table.grantFullAccess(submitCard);
-        table.grantFullAccess(onDisconnect);
-        table.grantFullAccess(defaultFunc);
-        table.grantFullAccess(resetRoom);
 
+        return table;
+    };
+
+    private grantTablePermissions(table: Table, functions: Record<string, NodejsFunction>): void {
+        Object.values(functions).forEach((func) => table.grantFullAccess(func));
     }
 }
