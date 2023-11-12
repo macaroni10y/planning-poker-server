a7aa91eb08b4cb36ea02bdebbc99e5762ff42793
kitajima.yuto <kitajima.yuto@furyu.jp>
2023-11-12 21:54:18 +0900

    stop background process when test exits

--- cdk/tests/integration/jest.globalSetup.ts
+++ cdk/tests/integration/jest.globalSetup.ts
@@ -1,4 +1,6 @@
 import {exec, execSync} from "child_process";
+import * as path from "path";
+import * as fs from "fs";
 
 module.exports = async () => {
     console.log('Starting DynamoDB Local...');
@@ -6,5 +8,6 @@ module.exports = async () => {
     console.log('Starting SAM Local...');
     execSync('npm run cdk synth -- --no-staging');
     console.log('Starting SAM Local api...');
-    exec('sam local start-api -t cdk.out/AdminApiStack.template.json --docker-network lambda-local');
+    const samLocal = exec('sam local start-api -t cdk.out/AdminApiStack.template.json --docker-network lambda-local');
+    fs.writeFileSync('sam-local.pid', samLocal!.pid!.toString());
 };
--- cdk/tests/integration/jest.globalTeardown.ts
+++ cdk/tests/integration/jest.globalTeardown.ts
@@ -1,6 +1,15 @@
 import {execSync} from "child_process";
+import * as path from "path";
+import * as fs from "fs";
 
 module.exports = async () => {
     console.log('Stopping DynamoDB Local...');
     execSync('docker stop $(docker ps -q --filter "ancestor=amazon/dynamodb-local")');
+    const pid = fs.readFileSync('sam-local.pid', 'utf8');
+    try {
+        execSync(`kill ${pid}`);
+        console.log(`Stopped SAM Local (PID: ${pid})`);
+    } catch (e) {
+        console.error(`Failed to stop SAM Local (PID: ${pid}): ${e}`);
+    }
 };
